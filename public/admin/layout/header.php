ca9b644aac068d0714668d4b5d1fc0cdd60fb7e8
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-08 14:50:52 +0200

    Header layout (temp)

--- public/admin/layout/header.php
+++ public/admin/layout/header.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $pageTitle ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type=""text/css" href="css/style.css"/>
+    <link rel="stylesheet" type=""text/css" href="../css/style.css"/>
 </head>
 <body>
