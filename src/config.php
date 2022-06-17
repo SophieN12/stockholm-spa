5791801fb436e3b06b5516942c47e7e02d0f4dda
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-17 18:41:52 +0200

    Added SRC paths

--- src/config.php
+++ src/config.php
@@ -9,3 +9,8 @@ define('ROOT_PATH', '..' . __DIR__ . '/'); // path to 'my-page-3/'
 define('SRC_PATH',  __DIR__ . '/'); // path to 'my-page-3/src/'
 
 // Include functions and classes
+
+require(SRC_PATH . '/dbconnect.php');
+require(SRC_PATH . '/app/common_functions.php');
+require(SRC_PATH . '/app/ProductsDbHandler.php');
+$productsDbHandler = new ProductsDbHandler($pdo);
