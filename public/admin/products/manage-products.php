deb38163816f8d2186d0eef722576f08d80cef4b
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-17 18:34:02 +0200

    Small changes and search functions

--- public/admin/products/manage-products.php
+++ public/admin/products/manage-products.php
@@ -6,51 +6,74 @@
 		$productsDbHandler -> deleteProduct($_POST['productId']);
     }
 
-	$products = $productsDbHandler->fetchAllProducts();
+    if (isset($_POST["submitSearch"])){
+		$searchResult =  trim($_POST['search-result']);
+		$sql = "
+			SELECT * FROM products 
+			WHERE title like :search
+		";
+
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':search', $searchResult .'%');
+		$stmt->execute();
+		$products = $stmt->fetchAll();
+
+    } else {
+		$products = $productsDbHandler -> fetchAllProducts();
+	}
 ?>
 
-<?php include("../layout/header.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?= $pageTitle ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/products.css"/>
+</head>
+<body>
 
 	<div class="container">
-		<h1>Admin Page</h1>
+		<h1> <a href="manage-products.php">Manage product</a> </h1>
 
-		<a href="add-product.php">Add new product +</a>
+		<div id="search-and-add-div">
+			<form id="search-form" class="d-flex" role="search" method="post" >
+				<input class="form-control me-2" id="search-bar" name="search-result" type="search" placeholder="Search by product name" aria-label="Search">
+				<input type="submit" class="btn" id="submit-search" name="submitSearch" value="Search">
+			</form>
+			<a href="add-product.php" id="add-product-btn">Add new product +</a>
+        </div>
 
 		<table id= "products-tbl">
 			<thead>
 				<tr>
-					<th>Img</th>
+					<th>IMG</th>
 					<th>ID</th>
-					<th>Title</th>
-					<th>Price</th>
-					<th>Stock</th>
-					<th>Actions</th>
+					<th>TITLE</th>
+					<th>PRICE</th>
+					<th>STOCK</th>
+					<th>ACTIONS</th>
 				</tr>
 			</thead>
 
 			<tbody>
-				<?php foreach ($products as $product) {
-					if (strlen($product['description']) >= 70){
-						$description = substr($product['description'], 0, 70) . '...';
-					} else {
-						$description = $product['description'];
-					}
-					?>
+				<?php foreach ($products as $product) { ?>
 					<tr>
-						<td width="130px"><img src=<?=$product['img_url']?> width="100px"></td>
+						<td><img src=<?=$product['img_url']?>></td>
 						<td><?= htmlentities($product['id']) ?></td>
-						<td><?= htmlentities($product['title']) ?></td>
+						<td class="title" data-id="<?=$product['id']?>"><?= htmlentities($product['title']) ?></td>
 						<td><?= htmlentities($product['price']) ?>:-</td>
 						<td><?= htmlentities($product['stock']) ?> st</td>
 						<td>
+							<form action="update-product.php" method="GET">
+								<input type="hidden" name="productId" value="<?= htmlentities($product['id']) ?>">
+								<input type="submit" name="updateProductBtn" value="Update">
+							</form>
 							<form action="" method="POST">
 								<input type="hidden" name="productId" value="<?= htmlentities($product['id']) ?>">
 								<input type="submit" name="deleteProductBtn" value="Delete">
-							</form>
-
-							<form action="update-product.php" method="GET">
-								<input type="hidden" name="productId" value="<?=htmlentities($product['id']) ?>">
-								<input type="submit" name="updateProductBtn" value="Update">
 							</form>
 						</td>
 					</tr>
@@ -58,5 +81,10 @@
 			</tbody>
 		</table> 
 	</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
     
-<?php include("../layout/footer.php") ?>
+    <footer> Copyrigth @ 2022 </footer>
+</body>
+</html>
