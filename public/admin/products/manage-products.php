25e25e6fe6433e69274c97db61a55cfdb8975860
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-17 18:39:06 +0200

    Small fix

--- public/admin/products/manage-products.php
+++ public/admin/products/manage-products.php
@@ -82,9 +82,4 @@
 		</table> 
 	</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-    
-    <footer> Copyrigth @ 2022 </footer>
-</body>
-</html>
+<?php include('../layout/footer.php'); ?>
