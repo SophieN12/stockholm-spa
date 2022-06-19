fa30487248d6785b36189920ef2f92a3c62ed56b
Natalie Esseen <natalieesseen@gmail.com>
2022-06-19 14:34:21 +0200

    additional styling

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-    require('../src/config.php');
-    $products = $productsDbHandler->fetchAllProducts();
+require('../src/config.php');
+$products = $productsDbHandler->fetchAllProducts();
 ?>
 
 <!DOCTYPE html>
@@ -8,21 +8,30 @@
 
 <head>
     <title>Stockholm Spa</title>
-    <link rel="stylesheet" href="../css/products.css">
+    <link rel="stylesheet" href="../css/products-page.css">
 </head>
 
 <body>
-	<h1>All products</h1>
-    <ul id="product-grid">
-        <?php foreach ($products as $product) { ?>
-            <li>
-                <a href="product.php?productId=<?= $product['id'] ?>">
-                    <img src="<?= htmlentities($product['img_url']) ?>" class="grid-img">
-                    <h2><?= htmlentities($product['title']) ?></h2>
-                    <p><?= htmlentities($product['price']) ?> SEK</p>
-                </a>
-            </li>
-        <?php } ?>
-    </ul>
+    <div class="container">
+        <h1>All products</h1>
+        <ul id="product-grid">
+            <?php foreach ($products as $product) { ?>
+                <li>
+                    <a href="product.php?productId=<?= $product['id'] ?>">
+                        <img src="<?= htmlentities($product['img_url']) ?>" class="grid-img">
+                        <div class="product-grid-info">
+                            <div class="product-grid-info-left">
+                                <h3><?= htmlentities($product['title']) ?></h3>
+                                <p><?= htmlentities($product['price']) ?> SEK</p>
+                            </div>
+                    </a>
+                    <div class="product-grid-info-right">
+                        <button>Add to cart</button>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
 </body>
+
 </html>
--- public/product.php
+++ public/product.php
@@ -1,17 +1,37 @@
 <?php
-    require('../src/config.php');
-    $product = $productsDbHandler->fetchProduct($_GET['productId']);
+require('../src/config.php');
+$product = $productsDbHandler->fetchProduct($_GET['productId']);
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Product</title>
-    <link rel="stylesheet" href="../css/products.css">
+    <title>Product</title>
+    <link rel="stylesheet" href="../css/products-page.css">
 </head>
+
 <body>
-        <img src="<?= htmlentities($product['img_url']) ?>" class="img-large">
-        <h2><?= htmlentities($product['title']) ?></h2>
-        <p><?= htmlentities($product['price']) ?></p>
+    <div class="container">
+        <div class="product-navigation">
+            <a href="index.php">
+                << Go back to all products</a>
+        </div>
+        <div class="product-description">
+            <div class="product-desc-left">
+                <img src="<?= $product['img_url'] ?>" class="img-large">
+            </div>
+            <div class="product-desc-right">
+                <h2><?= htmlentities($product['title']) ?></h2>
+                <div class="product-desc-mid-right">
+                    <p><?= htmlentities($product['price']) ?> SEK</p>
+                    <p><?= htmlentities($product['stock']) ?> in stock</p>
+                </div>
+                <p><?= htmlentities($product['description']) ?></p>
+                <button>Add to cart</button>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
