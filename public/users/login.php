f965cbbe35d2ff4d7676c10f2e908c13a6139a09
Pontus Björkdahl <pontus_pb95@hotmail.com>
2022-06-19 15:51:22 +0200

    added css to new-users and login

--- public/users/login.php
+++ public/users/login.php
@@ -9,27 +9,42 @@
     $incorrectLogin = "";
 
     if (isset($_GET['deletedUser'])) {
-        $deletedUser = "User deleted";
+        $deletedUser = 
+        '<div class="alert alert-danger" role="alert">
+            User deleted
+        </div>';
     }
 
     if (isset($_GET['logout'])) {
-        $logout = "User logged out";
+        $logout = 
+        '<div class="alert alert-danger" role="alert">
+            User logged out
+        </div>';
     }
 
     if (isset($_GET['mustLogin'])) {
-        $mustLogin = "Please login to access this page";
+        $mustLogin = 
+        '<div class="alert alert-danger" role="alert">
+            Please login to access this page
+        </div>';
     }
 
     if (isset($_POST['loginBtn'])) {
 
         if (empty($_POST['email'])) {
-            $errorMessages['email'] = "Please enter your email";
+            $errorMessages['email'] = 
+            '<div class="alert alert-danger" role="alert">
+                Please enter your email
+            </div>';
         } else {
             $email = trim($_POST['email']);
         }
         
         if (empty($_POST['password'])) {
-            $errorMessages['password'] = "Please enter your password";
+            $errorMessages['password'] = '
+            <div class="alert alert-danger" role="alert">
+                Please enter your password
+            </div>';
         } else {
             $password = trim($_POST['password']);
         }
@@ -46,7 +61,10 @@
                 redirect('my-page.php');
                 exit;
             } else {
-                $incorrectLogin = "The username or password you entered is incorrect";
+                $incorrectLogin = '
+                <div class="alert alert-danger" role="alert">
+                    The username or password you entered is incorrect
+                </div>';
             }
         }
     }
@@ -61,19 +79,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/general.css">
     <title>Login</title>
 </head>
 <body>
     <div id="container">
         <h1>Login</h1>
-        <div class="alert alert-danger" role="alert">
             <p><?=$deletedUser?></p>
             <p><?=$logout?></p>
             <p><?=$mustLogin?></p>
             <p><?=$incorrectLogin?></p>
             <p><?=$errorMessages['email']?></p>
             <p><?=$errorMessages['password']?></p>
-        </div>
         <form action="" method="POST">
             <div id="form-inputs">
                 <label for="email">Email*</label><br>
@@ -81,11 +98,16 @@
                 <label for="password">Password*</label><br>
                 <input type="password" name="password"><br>
             </div>
-            <input id="loginBtn" type="submit" name="loginBtn" value="Login">
+            <input id="submitBtn" type="submit" name="loginBtn" value="Login">
         </form><br>
         <p id="sign-up">Not a member yet? <a id="sign-up-link" href="new-user.php">Sign up here!</a></p>
         <a href="new-user.php">Create new account</a><br><br>
         <a href="my-page.php">My page</a>
     </div>
+    <!-- jQuery AND Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
 </body>
 </html>
--- public/users/new-user.php
+++ public/users/new-user.php
@@ -20,6 +20,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/new-user.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Create new user</title>
 </head>
@@ -28,53 +31,58 @@
         <h1>Create new user</h1>
         <p id="successMessage"></p>
         <form id="add-user-form" action="" method="POST">
-            <label for="first_name">First Name</label><br>
-            <input type="text" name="first_name"><br>
-            <p id="first-name-error"></p>
-            
-            <label for="last_name">Last Name</label><br>
-            <input type="text" name="last_name"><br>
-            <p id="last-name-error"></p>
-            
-            <label for="street">Street</label><br>
-            <input type="text" name="street"><br>
-            <p id="street-error"></p>
-            
-            <label for="postal_code">Postal Code</label><br>
-            <input type="text" name="postal_code"><br>
-            <p id="postal_code-error"></p>
-            
-            <label for="email">Email</label><br>
-            <input type="text" name="email"><br>
-            <p id="email-error"></p>
-            <p id="email-error2"></p>
-            
-            <label for="password">Password</label><br>
-            <input type="password" name="password"><br>
-            <p id="password-error"></p>
-            
-            <label for="confirmPassword">Confirm password</label><br>
-            <input type="password" name="confirmPassword"><br>
-            <p id="confirmPassword-error"></p>
-            
-            <label for="phone">Phone number</label><br>
-            <input type="text" name="phone"><br>
-            <p id="phone-error"></p>
-            
-            <label for="city">City</label><br>
-            <input type="text" name="city"><br>
-            <p id="city-error"></p>
-            
-            <label for="country">Country</label><br>
-            <select name="country" id="country">
-                <option disabled selected value> -- select a country -- </option>
-                <option value="SE">Sweden</option>
-                <option value="NO">Norway</option>
-                <option value="DK">Denmark</option>
-                <option value="FI">Finland</option>
-            </select><br><br>
-            <p id="country-error"></p>
-            <input type="submit" name="createBtn" value="Create" id="submitBtn">
+            <div id="form-inputs">
+                <div id="left-side-form">
+                    <label for="first_name">First Name*</label><br>
+                    <input type="text" name="first_name"><br>
+                    <p id="first-name-error"></p>
+                    
+                    <label for="last_name">Last Name*</label><br>
+                    <input type="text" name="last_name"><br>
+                    <p id="last-name-error"></p>
+                    
+                    <label for="email">Email*</label><br>
+                    <input type="text" name="email"><br>
+                    <p id="email-error"></p>
+                    <p id="email-error2"></p>
+    
+                    <label for="password">Password*</label><br>
+                    <input type="password" name="password"><br>
+                    <p id="password-error"></p>
+                    
+                    <label for="confirmPassword">Confirm password*</label><br>
+                    <input type="password" name="confirmPassword"><br>
+                    <p id="confirmPassword-error"></p>
+                </div>
+                <div id="right-side-form">
+                    <label for="street">Street*</label><br>
+                    <input type="text" name="street"><br>
+                    <p id="street-error"></p>
+                    
+                    <label for="postal_code">Postal Code*</label><br>
+                    <input type="text" name="postal_code"><br>
+                    <p id="postal_code-error"></p>
+                    
+                    <label for="phone">Phone number*</label><br>
+                    <input type="text" name="phone"><br>
+                    <p id="phone-error"></p>
+                    
+                    <label for="city">City*</label><br>
+                    <input type="text" name="city"><br>
+                    <p id="city-error"></p>
+                    
+                    <label for="country">Country*</label><br>
+                    <select name="country" id="country">
+                        <option disabled selected value> Select a country</option>
+                        <option value="SE">Sweden</option>
+                        <option value="NO">Norway</option>
+                        <option value="DK">Denmark</option>
+                        <option value="FI">Finland</option>
+                    </select><br><br>
+                    <p id="country-error"></p>
+                </div>
+            </div>
+            <input id="submitBtn" type="submit" name="createBtn" value="Create" id="submitBtn">
         </form><br>
         <a href="login.php">Login page</a><br><br>
         <a href="../index.php">&#8592 Back</a>
