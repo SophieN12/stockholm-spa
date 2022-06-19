91cf22597bf527954ab4138cd890cc210e7ff3bc
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-19 16:27:03 +0200

    Moved to right folder

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+// Turn on/off error reporting
+error_reporting(-1);
+
+// Start session
+session_start();
+
+define('ROOT_PATH', '..' . __DIR__ . '/'); // path to 'my-page-3/'
+define('SRC_PATH',  __DIR__ . '/'); // path to 'my-page-3/src/'
+
+// Include functions and classes
+
+require(SRC_PATH . '/dbconnect.php');
+require(SRC_PATH . '/app/common_functions.php');
+require(SRC_PATH . '/app/ProductsDbHandler.php');
+$productsDbHandler = new ProductsDbHandler($pdo);
