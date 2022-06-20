17635fd96d622586c1a08bade03f5b28e1fe7695
Pontus Björkdahl <pontus_pb95@hotmail.com>
2022-06-20 13:26:36 +0200

    removed function

--- src/app/userFunctions.php
+++ src/app/userFunctions.php
@@ -1,9 +1,4 @@
 <?php
-    function redirect($path) {
-        header("Location: {$path}");
-        exit;
-    }
-
     function destroySession () {
         $_SESSION = [];
         session_destroy();
