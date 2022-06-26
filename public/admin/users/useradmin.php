f533af758bf212971e61a2e9c883e74c42cb1947
louise ernvik <louiseernvik@gmail.com>
2022-06-26 20:30:55 +0200

    error message done

--- public/admin/users/useradmin.php
+++ public/admin/users/useradmin.php
@@ -98,7 +98,7 @@ $users = $useradminDbHandler->fetchAllUsers();
 		<div class="new-user-wrapper">
 
 			<h1 class="rubrik">Create new user</h1>
-			<div id="form-message" > <?=$message?> </div>
+			<div id="form-message" class="error"><?=$message?></div>
 
 			<!-- ny användare steg 2 -->
 			<form id="add-user-form" action="add-user.php" method="POST">
