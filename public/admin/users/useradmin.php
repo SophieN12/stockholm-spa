b5a3b8e7b084309ac6f2adc9c76d19234eb0c3a2
louise ernvik <louiseernvik@gmail.com>
2022-06-29 12:58:02 +0200

    deleted comments and useless code

--- public/admin/users/useradmin.php
+++ public/admin/users/useradmin.php
@@ -4,88 +4,12 @@ $pageTitle = "Admin Users";
 
 $message	="";
 $successmessage	="";
-// $message	="";
-// $error		="";
-// $first_name	="";
-// $last_name	="";
-// $email		="";
-// $passowrd	="";
-// $phone		="";
-// $street		="";
-// $postal_code="";
-// $city		="";
-// $country	="";
-// // Add user with validation
-// if(isset($_POST['addUserBtn'])) {
 
-// 	$first_name		=trim($_POST['first_name']);
-// 	$last_name		=trim($_POST['last_name']);
-// 	$email			=trim($_POST['email']);
-// 	$passowrd		=trim($_POST['password']);
-// 	$phone			=trim($_POST['phone']);
-// 	$street			=trim($_POST['street']);
-// 	$postal_code	=trim($_POST['postal_code']);
-// 	$city			=trim($_POST['city']);
-// 	$country		=trim($_POST['country']);
-
-// 	if (empty($first_name)) {
-// 		$error .= "Förnamn är obligatoriskt <br>";
-// 	}
-// 	if (empty($last_name)) {
-// 		$error .= "Efternamn är obligatoriskt <br>";
-// 	}
-// 	if (empty($email)) {
-// 		$error .= "Email är obligatoriskt <br>";
-// 	}
-// 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-// 		$error .= "Ogiltig e-post adress<br>";
-// 	}
-// 	if (empty($passowrd)) {
-// 		$error .= "Lösenord är obligatoriskt <br>";
-// 	}
-// 	if (empty($phone)) {
-// 		$error .= "Mobilnummer är obligatoriskt <br>";
-// 	}
-// 	if (empty($street)) {
-// 		$error .= "Adress är obligatoriskt <br>";
-// 	}
-// 	if (empty($postal_code)) {
-// 		$error .= "Postnummer är obligatoriskt <br>";
-// 	}
-// 	if (!filter_var($postal_code, FILTER_VALIDATE_INT)) {
-// 		$error .= "Ogiltigt postnummer <br>";
-// 	}
-// 	if (empty($city)) {
-// 		$error .= "Stad är obligatoriskt <br>";
-// 	}
-// 	if (empty($country)) {
-// 		$error .= "Land är obligatoriskt <br>";
-// 	}
-
-// 	if ($error) {
-// 		$message = "
-// 			<div>
-// 				{$error}
-// 			</div>
-// 		";
-// 	} else {
-// 		$useradminDbHandler->addUser($first_name, $last_name, $email, $password, $phone, $street, $postal_code, $city, $country);
-// 	}
-// }
-
-
-
-// Delete user
 if(isset($_POST['deleteUserBtn'])){
 	$useradminDbHandler->deleteUser();
 }
 
-// Hämtar ALLA
 $users = $useradminDbHandler->fetchAllUsers();
-
-// echo "<pre>";
-// print_r($users);
-// echo "</pre>";
 
 ?>
 
@@ -102,7 +26,6 @@ $users = $useradminDbHandler->fetchAllUsers();
 			<div id="form-message" class="error"><?=$message?></div>
 			<div id="form-message-success" class="success"><?=$successmessage?></div>
 
-			<!-- ny användare steg 2 -->
 			<form id="add-user-form" action="add-user.php" method="POST">
 
 				<div id="field-wrapper">
@@ -133,9 +56,7 @@ $users = $useradminDbHandler->fetchAllUsers();
 					<label for="">City</label>
 					<input type="text" class="inputfield" name="city" placeholder="City">
 				</div>
-					<!-- <input type="text" class="" name="country" placeholder="Country"> -->
 					
-					<!-- <label for="country">Välj land</label> <br> -->
 				<div id="field-wrapper">
 					<label for="">Choose Country</label>
 					<select class="inputfield" name="country" id="country">
