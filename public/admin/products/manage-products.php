ae8bd2be67be0964219a66a84fca08030bfaf3dc
SophieN12 <Sophie-nhan@hotmail.com>
2022-06-29 10:16:47 +0200

    Removed an unnecessary link

--- public/admin/products/manage-products.php
+++ public/admin/products/manage-products.php
@@ -1,6 +1,6 @@
 <?php 
 	$pageId = "admin";
-    $pageTitle = "Admin Page";
+    $pageTitle = "Admin Products";
     require('../../../src/config.php');
 
     if (isset($_POST["submitSearch"])){
@@ -21,7 +21,6 @@
 ?>
 
 <?php include('../layout/header.php'); ?>	
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
 <link rel="stylesheet" type=""text/css" href="../css/products.css"/>
 
 	<div class="container">
