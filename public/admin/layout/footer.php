617dc69efcf1775d6d0d0b983505473434251fb7
Pontus Björkdahl <pontus_pb95@hotmail.com>
2022-06-29 11:34:25 +0200

    changed to english in footer

--- public/admin/layout/footer.php
+++ public/admin/layout/footer.php
@@ -1,24 +1,24 @@
 <footer>
     <section id="opening-hours">
-        <h2 class="footer-h2">ÖPPETTIDER</h2>
+        <h2 class="footer-h2">OPENING HOURS</h2>
         <div id="opening-hours-text">
-            <p class="footer-p" id="weekdays-text">Måndag - Fredag</p>
+            <p class="footer-p" id="weekdays-text">Monday - Friday</p>
             <p class="footer-p">09 - 22</p>
-            <p class="footer-p" id="saturday-text">Lördag</p>
+            <p class="footer-p" id="saturday-text">Saturday</p>
             <p class="footer-p">10 - 22</p>
-            <p class="footer-p" id="sunday-text">Söndag</p>
+            <p class="footer-p" id="sunday-text">Sunday</p>
             <p class="footer-p">11 - 23</p>
         </div>
     </section>
 
     <section id="find-us-here">
-        <h2 class="footer-h2">HITTA HIT</h2>
+        <h2 class="footer-h2">FIND US</h2>
         <p class="footer-p">Mångatan 167<br>Stockholm</p>
         <img src="../img/img-footer/location-icon.png" alt="A Location Icon">
     </section>
 
     <section id="contact-info">
-        <h2 class="footer-h2">KONTAKT</h2>
+        <h2 class="footer-h2">CONTACT</h2>
         <p class="footer-p">Stockholm@spa.com<br>08-8936763</p>
         <img src="../img/img-footer/insta-icon.png" alt="Instagram's Icon">
         <img src="../img/img-footer/facebook-icon.png" alt="Facebook's Icon">
